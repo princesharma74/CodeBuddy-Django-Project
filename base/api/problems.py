2170b6abff2dcf4ac0a7bca72a5f0893bc5c46d5
Prince Sharma <princesharma2899@gmail.com>
2024-04-09 16:10:24 +0530

    organised apis

---
+++ base/api/problems.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from base.models import Room, User, Problem, Topic, Submission
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
+from .serializers import RoomSerializer, UserSerializer, ProblemSerializer, TopicSerializer, SubmissionSerializer, UserLoginSerializer
+
+@api_view(['GET'])
+def getProblems(request):
+    problems = Problem.objects.all()
+    serializer = ProblemSerializer(problems, many=True)
+    return Response(serializer.data)
