17e4d230c43252e59e4f57322341137977f6e0f6
Prince Sharma <princesharma2899@gmail.com>
2024-04-10 16:21:10 +0530

    Removed errorsg

--- base/api/problems.py
+++ base/api/problems.py
@@ -9,6 +9,7 @@ def getProblems(request):
     serializer = ProblemSerializer(problems, many=True)
     return Response(serializer.data)
 
+@api_view(['POST'])
 def getProblemsByUser(request, pk):
     problems = Problem.objects.filter(submitted_by=pk)
     serializer = ProblemSerializer(problems, many=True)
